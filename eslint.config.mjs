1351bad9f3be0cec8d6c2a5faa5d78837693f512
La Thành Thái (Samuel La) <lathanhthai1905@gmail.com>
2024-06-12 00:01:33 +0700

    Change the Source File when Build Project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,17 @@ import pluginJs from '@eslint/js';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
+	pluginJs.configs.recommended,
+	pluginReactConfig,
 	{
 		languageOptions: {
-			globals: globals.browser,
+			globals: { __dirname: true, ...globals.browser },
 		},
 	},
-	pluginJs.configs.recommended,
 	{
 		files: ['**/*.jsx'],
 		languageOptions: {
 			parserOptions: { ecmaFeatures: { jsx: true } },
 		},
 	},
-	pluginReactConfig,
 ];
