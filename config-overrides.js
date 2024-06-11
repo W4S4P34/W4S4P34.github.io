0d3b96e455c417739f3abd4e6861075a27e37d06
La Thành Thái (Samuel La) <lathanhthai1905@gmail.com>
2024-06-12 00:08:45 +0700

    Update config for Github Action

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-import { resolve } from 'path';
-
-export function paths(paths) {
-	paths.appIndexJs = resolve(__dirname, 'source/index.js');
-	paths.appSrc = resolve(__dirname, 'source');
-	return paths;
-}
