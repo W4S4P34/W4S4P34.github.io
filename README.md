6ba9b7331169dbabbac6c09d5203ac2ae9b69bdb
La Thành Thái (Samuel La) <lathanhthai1905@gmail.com>
2024-08-05 22:13:21 +0700

    Put Project under Maintenance

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <div id="Header" align="center">
-  <h3 align="center"> 🌱 W4S4P34's PORTFOLIO 🌱 </h3>
+	<h3 align="center"> 🌱 W4S4P34's PORTFOLIO 🌱 </h3>
 </div>
--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 // React
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-	return <h1>Hello, World!</h1>;
+	const ORIGINAL_TITLE = "W4S4P34'S PORTFOLIO";
+
+	const [title, setTitle] = useState(ORIGINAL_TITLE);
+	const [titleHovering, setHoverTitle] = useState(false);
+
+	const onMouseEnter = () => {
+		setHoverTitle(true);
+	};
+	const onMouseLeave = () => {
+		setHoverTitle(false);
+	};
+
+	useEffect(() => {
+		if (titleHovering) {
+			setTitle(ORIGINAL_TITLE);
+		} else {
+			const interval = setInterval(() => {
+				const shuffledCharacters = title
+					.split('')
+					.sort(() => 0.5 - Math.random())
+					.join('');
+				setTitle(shuffledCharacters);
+			}, 10);
+			return () => clearInterval(interval);
+		}
+	}, [title, titleHovering]);
+
+	return (
+		<div className='flex flex-col w-screen self-center justify-self-center font-mono font-bold text-3xl text-center select-none'>
+			<h3
+				onMouseEnter={onMouseEnter}
+				onMouseLeave={onMouseLeave}>
+				{title}
+			</h3>
+			<h3>
+				This website is currently under maintenance, please comeback
+				later
+			</h3>
+		</div>
+	);
 };
 
 export default App;
